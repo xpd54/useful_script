517ee19a38768e4d944831b98ce1ccbcc1a48d4f
Ravi Prakash <raviprakash.xpd54@gmail.com>
2016-06-28 13:21:31 +0530

    Add page scroller till given number of end time

--- facebookScrap.py
+++ facebookScrap.py
@@ -1,21 +1,27 @@
 
 import time
 from selenium import webdriver
-#get the we driver 
-driver = webdriver.Chrome('path of chromedriver')  # Optional argument, if not specified will search path.
-
+#get the web driver
+driver = webdriver.Chrome('path to chromedriver')
 
 driver.get('http://facebook.com');
 email = driver.find_element_by_name('email')
 password = driver.find_element_by_name('pass')
-email.send_keys('userid')
-password.send_keys('password')
+email.send_keys('your facebook id')
+password.send_keys('your facebook password')
 driver.find_element_by_id('loginbutton').click()
-
-driver.get('group url')
+driver.get('link of group you wanna get the posts')
+page = 0
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    page = page + 1
+    print 'Page Number ',page
+    if page == 'Number of time you wanna scroll and then get the content': #will come back with better solution
+            break
+    time.sleep(1)
 postContent = driver.find_elements_by_id('contentArea')
 for content in postContent:
-	pageGroup = content.find_elements_by_id('pagelet_group_mall')
+        pageGroup = content.find_elements_by_id('pagelet_group_mall')
 	for page in pageGroup:
 		userContent = page.find_elements_by_class_name('_3ccb')
 		for element in userContent:
